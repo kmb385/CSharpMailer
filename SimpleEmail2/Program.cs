8c49540a55aaac89dd03fa7d3d54089ceea22ed4
Kevin Bowersox <kmb385@gmail.com>
2018-01-03 09:18:58 -0500

    Initial

---
+++ SimpleEmail2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+
+namespace SimpleEmail2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string customerName = "Mrs. Warren";
+            string medicationName = "Advil";
+            string quantity = "3";
+            string unitOfMeasure = "Boxes";
+
+            MailAddress toAddress = new MailAddress("kmb385@gmail.com");
+            MailAddress fromAddress = new MailAddress("kmb385@gmail.com");
+            MailMessage mailMessage = new MailMessage(fromAddress, toAddress);
+            mailMessage.IsBodyHtml = true;
+            mailMessage.Subject = $"New Medication Order for {customerName}";
+            mailMessage.Body = "<p>A new order has arrived.</p>" +
+                    $"<p>{customerName} has ordered {quantity} {unitOfMeasure} of {medicationName}";
+
+
+            SimpleMailer mailer = new SimpleMailer();
+            mailer.send(mailMessage);
+        }
+    }
+}
---
+++ SimpleEmail2/SimpleMailer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
+using System.Configuration;
+
+
+namespace SimpleEmail2
+{
+    class SimpleMailer
+    {
+
+        private MailAddress fromMailAddress { get; set; }
+        private MailAddress toMailAddress { get; set; }
+
+        private int port = Int32.Parse(ConfigurationManager.AppSettings.Get("port"));
+        private string host = ConfigurationManager.AppSettings.Get("host");
+        private string user = ConfigurationManager.AppSettings.Get("user");
+        private string password = ConfigurationManager.AppSettings.Get("password");
+
+        private string subject { get; set; }
+        private string body { get; set; }
+
+        SmtpClient smtpClient = new SmtpClient();
+
+        public SimpleMailer()
+        {
+            smtpClient.Port = port;
+            smtpClient.Host = host;
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.Credentials = new NetworkCredential(user, password);
+            smtpClient.EnableSsl = true;
+            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+        }
+
+        public void send(MailMessage message)
+        {
+            try
+            {
+                smtpClient.Send(message);
+            } catch(Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if(message != null)
+                {
+                    message.Dispose();
+                }
+            }
+        }
+
+    }
+}
